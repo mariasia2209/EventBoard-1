5a36822499525f22abb504dac651387bf3d8a722
orchidoris <martahryvniak@gmail.com>
2016-09-27 00:10:42 +0300

    Initial Commit

---
+++ EventBoard.Presentation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventBoard.Presentation.MvcApplication" Language="C#" %>
---
+++ EventBoard.Presentation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventBoard.Presentation.Startup))]
+namespace EventBoard.Presentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
